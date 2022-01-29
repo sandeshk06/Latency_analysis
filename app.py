957ef623636b0e7438d59a839aca0c78116f5feb
sandeshkulkarni <sandeshkulkarni1991@gmail.com>
2022-01-29 13:50:32 +0000

    liked geo location trace  to app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from check_domain_result import compute
 from check_traceroute import get_traceroute_result 
 from check_mtr import get_mtr_result 
 from check_ping import get_ping_result 
+from geo_latency_info import *
 from waitress import serve
 
 app = Flask(__name__)
@@ -40,6 +41,21 @@ def traceroute():
             traceroute_result=get_traceroute_result(name)
             mtr_result=get_mtr_result(name)
             ping_result=get_ping_result(name)
+            
+            #draw trace on map
+            Cordinate_list=[]
+            geo_obj=GeoTrace(name)
+            SOURCE_LOCATION,TRACES=geo_obj.get_geo_traceroute_data()
+            
+            Cordinate_list.append(list(SOURCE_LOCATION))
+
+            if TRACES:
+                for hop,cordinate in sorted(TRACES.items()):
+                    Cordinate_list.append(cordinate)
+
+            
+
+
             return render_template('show_traceroute_result.html',url=name,traceroute_result=traceroute_result,mtr_result=mtr_result,ping_result=ping_result)
 
 
